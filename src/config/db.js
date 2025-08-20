5aedb7de962341c28fa8d51aa503b9f37e90ac19
Muhamad Nur Rizki <farianhab123@gmail.com>
2025-08-20 10:57:05 +0700

    menambahkan port di configurasi db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql.createPool({
+  port: Number(process.env.PORT),
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
