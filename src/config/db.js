b409cbdbc407dc2035e4a4baec2736b69017c174
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-03-14 20:38:49 +0700

    maintenancd backend

--- src/config/db.js
+++ src/config/db.js
@@ -2,17 +2,15 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const pool = mysql.createPool({
-  port: Number(process.env.PORT),
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  multipleStatements: true,
-  waitForConnections: true,
+  port: isProduction ? Number(process.env.PORT) : 3306,
+  host: isProduction ? process.env.HOST : "localhost",
+  user: isProduction ? process.env.USER : "root",
+  password: isProduction ? process.env.PASSWORD : "1234",
+  database: isProduction ? process.env.DATABASE : "money_tracking",
+  multipleStatements: false,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
